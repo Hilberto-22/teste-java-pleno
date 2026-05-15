924464a2378c58c87c3669bb5db640c9c3d4e2cd
Hilberto Fernandes <betogouveia1@outlook.com>
2026-05-15 17:05:01 -0300

    feat: Add swagger

---
+++ src/main/java/com/hilberto/teste/infrastructure/config/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package com.hilberto.teste.infrastructure.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Teste técnico")
+                        .version("v1")
+                        .description("Teste técnico para vaga de Desenvolvedor Java Pleno")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+    }
+}
