4ba2e14a10c745a12a45cc4a8bdc6f624974a99b
Hilberto-22 <betogouveia1@outlook.com>
2026-05-15 19:08:38 -0300

    add responseStatus in CouponController.java

--- src/main/java/com/hilberto/teste/resources/controller/CouponController.java
+++ src/main/java/com/hilberto/teste/resources/controller/CouponController.java
@@ -3,6 +3,7 @@ package com.hilberto.teste.resources.controller;
 import com.hilberto.teste.domain.dto.request.CouponRequest;
 import com.hilberto.teste.domain.dto.response.CouponResponse;
 import com.hilberto.teste.domain.service.CouponService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,11 +20,13 @@ public class CouponController {
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<CouponResponse> createCoupon(@RequestBody CouponRequest request){
         return ResponseEntity.ok(couponService.create(request));
     }
 
     @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable UUID id){
         couponService.delete(id);
     }
